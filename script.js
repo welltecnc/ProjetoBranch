6512e6fcd43ec984ce43c6694b8c6d31bd66c3c8
Wellington <welltecnic@hotmail.com>
2024-10-03 11:32:46 -0300

    Criando array, objeto e metodos

--- script.js
+++ script.js
@@ -23,13 +23,30 @@ while(w<5);
 function Ola(nome){
     // `` ( strings literal ou template string ${} chama a variavel)
      console.log(`Seja bem vindo a ${nome}`)
-      alert(`seja bem vindo a ${nome}`)
+      //alert(`seja bem vindo a ${nome}`)
       document.write(`seja bem vindo a ${nome}`)
 }
 //executando a função Ola passando o valor 
 Ola("fiap")
 
+//Arrow Function
 
+const hello=()=>(console.log("função arrow function"))
+
+hello()
+
+//ARRAY [], OBJETO {} E METODOS ()
+
+let jogadores =[
+    {nome:"Huguinho", idade:17},
+    {nome:"Zezinho",idade:18},
+    {nome:"Luizinho",idade:19}
+]
+
+let listarJogadores = jogadores.map(function(item){
+    return item.idade;
+})
+console.log(listarJogadores)
 
 
 
